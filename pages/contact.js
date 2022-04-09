c5df8d4c38e53d06428435116a18d14f5b9a0e9a
arshad-qadri <arshadqadri321@gmail.com>
2022-04-09 11:05:06 +0000

    anchor tabg changes in contact

--- pages/contact.js
+++ pages/contact.js
@@ -20,22 +20,26 @@ const Contact = () => {
           <div className="contacts">
             <div className="row gy-3">
               <div className="col-lg-6 col-md-6 col-sm-12">
-                <a href="mailto:arshadqadri321@gmail.com">
-                  <span>Email : </span>arshadqadri321@gmail.com
-                </a>
+                <div className="w-100 contact">
+                  <a href="mailto:arshadqadri321@gmail.com">
+                    <span>Email : </span>arshadqadri321@gmail.com
+                  </a>
+                </div>
               </div>
               <div className="col-lg-6 col-md-6 col-sm-12">
-                <a href="tel:8007330423">
-                  {" "}
-                  <span>Phone : </span> +918007330423{" "}
-                </a>
+                <div className="w-100 contact">
+                  <a href="tel:8007330423">
+                    <span>Phone : </span> +918007330423
+                  </a>
+                </div>
               </div>
               <div className="col-lg-12 col-md-12 col-sm-12">
-                <a href="#">
-                  {" "}
-                  <span>Address : </span> Prakshnagar, Kupwad road, Sangli
-                  416416.{" "}
-                </a>
+                <div className="w-100 contact bg-danger">
+                  <a href="#">
+                    <span>Address : </span> Prakshnagar, Kupwad road, Sangli
+                    416416.
+                  </a>
+                </div>
               </div>
             </div>
           </div>
