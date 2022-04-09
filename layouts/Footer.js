82baa2b5747ecf71224e0e6d9556e4a3d81a7b33
arshad-qadri <49265079+arshad-qadri@users.noreply.github.com>
2022-04-09 20:38:50 +0530

    Update Footer.js
    Copyright year changed

--- layouts/Footer.js
+++ layouts/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className=" footer w-100 text-center">
-      <p className="m-0">Copyright © 2018</p>
+      <p className="m-0">Copyright © 2022</p>
     </div>
   );
 };
