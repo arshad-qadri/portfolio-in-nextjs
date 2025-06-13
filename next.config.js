e3d195a4df93147c772f0ffb6ca79837697c498a
Arshad Qadri <mohammadarshadraza.mohammadqayyum@neosoftmail.com>
2025-06-13 09:59:34 +0530

    _redirects  added

--- next.config.js
+++ next.config.js
@@ -4,15 +4,6 @@ const nextConfig = {
   images: {
     domains: ["images.pexels.com", "images.unsplash.com", "i.postimg.cc"],
   },
-  async redirects() {
-    return [
-      {
-        source: "https://arshad-01.netlify.app/", // this catches the root path
-        destination: "https://arshadqadri.com/",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
